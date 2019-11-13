e5e923cf6fa2d4b82f58a9e715a73e4375ad1f1a
roman bogdanov <cflatminor92@gmail.com>
2019-11-13 19:27:14 +0200

    basic layout

--- gulpfile.js
+++ gulpfile.js
@@ -27,11 +27,31 @@ function startBrowserSync () {
   });
 }
 
-function compileScss () {
-  return src('app/scss/index.scss', { allowEmpty: true })
+/**
+  * @method compileScss
+  * @param source {String}
+  * @param dist {String}
+  */
+function compileScss (source, dist) {
+  return src(source)
     .pipe(sass().on('error', sass.logError))
+    // .pipe(autoprefixer())
     .pipe(concat('main.css'))
-    .pipe(dest('dist'));
+    .pipe(dest(dist));
+}
+
+function buildApp () {
+  let date = new Date();
+  let page = 'App/Framework'; // todo param
+  compileScss('app/scss/framework/index.scss', 'dist/App');
+  return console.log (`${page} compiled successfully at ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+}
+
+function buildHomePage () {
+  let date = new Date();
+  let page = 'HomePage'; // todo param
+  compileScss('app/scss/HomePage/index.scss', 'dist/HomePage');
+  return console.log (`${page} compiled successfully at ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
 }
 
 function compileJs () {
@@ -40,16 +60,21 @@ function compileJs () {
     .pipe(dest('dist'))
 }
 
+// ---------------- BUILD PROJECT ---------------- //
+async function buildProject () {
+  buildApp();
+  await buildHomePage();
+}
+
 function watchFiles () {
   startBrowserSync();
 
   watch('index.html').on('change', browserSync.reload);
 
-  watch('app/scss/**/*.scss').on('change', compileScss);
-  watch('dist/main.css').on('change', browserSync.reload);
+  watch('app/scss/**/*.scss').on('change', series(buildProject, browserSync.reload));
 
-  watch('app/js/**/*.js').on('change', compileJs);
-  watch('dist/main.js').on('change', browserSync.reload);
+  // watch('app/js/**/*.js').on('change', compileJs);
+  // watch('dist/main.js').on('change', browserSync.reload);
 }
 
-exports.watch = series(deleteDistFolder, parallel(compileScss, compileJs), watchFiles);
+exports.watch = series(buildProject, watchFiles);
